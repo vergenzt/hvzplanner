87e34b7d8e751c72a125d54eed0f695907150f6c
Tim Vergenz <vergenzt@gmail.com>
2013-11-22 16:16:59 -0500

    Refactor example probability maps

--- src/main/java/zombieplanner/GTMapGenerator.java
+++ src/main/java/zombieplanner/GTMapGenerator.java
@@ -26,15 +26,43 @@ public final class GTMapGenerator {
 		}
 		return gtMap;
 	}
-	
+
 	public static ProbabilityMap loadGTZombieProbabilities() {
 		if (probMap == null) {
-			probMap = new ProbabilityMap(false, WIDTH, HEIGHT);
-			for (int i=0; i<probMap.getData().length; i++)
-				probMap.getData()[i] = 1.0;
-			probMap.normalize();
+			probMap = getUniformProbabilityMap(WIDTH, HEIGHT);
 			// TODO load zombie probabilities
 		}
+		return probMap;
+	}
+
+	/**
+	 * Get a ProbabilityMap where every cell has equal probability.
+	 * @param width
+	 * @param height
+	 * @return a normalized equal-weighted probability map
+	 */
+	public static ProbabilityMap getUniformProbabilityMap(int width, int height) {
+		ProbabilityMap probMap = new ProbabilityMap(false, WIDTH, HEIGHT);
+		for (int i=0; i<probMap.getData().length; i++)
+			probMap.getData()[i] = 1.0;
+		probMap.normalize();
+		return probMap;
+	}
+
+	/**
+	 * Get a ProbabilityMap where probability is proportional to Manhatten distance
+	 * from the center point.
+	 * @param width
+	 * @param height
+	 * @return a normalized centralized probability map
+	 */
+	public static ProbabilityMap getCentralizedProbabilityMap(int width, int height) {
+		ProbabilityMap probMap = new ProbabilityMap(false, WIDTH, HEIGHT);
+		int ci = (width/2), cj = (height/2);
+		for (int i=0; i<probMap.size(0); i++)
+			for (int j=0; j<probMap.size(1); j++)
+				probMap.set(Math.abs(i-ci) + Math.abs(j-cj), i, j);
+		probMap.normalize();
 		return probMap;
 	}
 
